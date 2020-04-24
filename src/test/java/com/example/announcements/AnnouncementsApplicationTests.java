a998d7bf61f50bc11c6b7fe1efa52889fc032db4
Urszula Sołogub <gold.ula4@gmail.com>
2020-04-25 00:54:15 +0200

    Initial commit

---
+++ src/test/java/com/example/announcements/AnnouncementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.announcements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnouncementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
