61bb28d01fd5f904b27410ff812880091bbbdd10
Urszula Sołogub <gold.ula4@gmail.com>
2020-05-25 17:51:25 +0200

    Deleted duplicated files

--- src/main/java/com/example/announcements/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.announcements.service;
-
-import com.example.announcements.models.User;
-
-public interface UserService {
-
-	public User saveUser(User user);
-	public boolean isUserAlreadyPresent(User user);
-}
