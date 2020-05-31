1fc7cc7d4a0f935bb852d110f6ee150920a6cce3
Urszula Sołogub <gold.ula4@gmail.com>
2020-05-31 22:06:18 +0200

    Added uploading images

--- API/src/main/java/com/example/announcements/SecurityConfiguration.java
+++ API/src/main/java/com/example/announcements/SecurityConfiguration.java
@@ -77,6 +77,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/admin_priv").hasAnyAuthority("ADMIN_USER")
 				.antMatchers("/home/**").hasAnyAuthority( "ADMIN_USER", "SITE_USER")
 				.antMatchers("/home").permitAll()
+				.antMatchers("/upload").permitAll()
 				.antMatchers("/announcements").permitAll()
 				.antMatchers("/announcements/category/{id}").permitAll()
 				.antMatchers("/announcements/{id}").permitAll()
---
+++ API/src/main/java/com/example/announcements/controllers/RestImagesController.java
@@ -0,0 +1,30 @@
+package com.example.announcements.controllers;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@SpringBootApplication
+@RestController
+public class RestImagesController  {
+
+	@RequestMapping(value="/upload", method= RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+		File convertFile = new File("src/main/resources/images/" + file.getOriginalFilename());
+		convertFile.createNewFile();
+		FileOutputStream fout = new FileOutputStream(convertFile);
+		fout.write(file.getBytes());
+		fout.close();
+		return new ResponseEntity<>("File is uploaded successfully", HttpStatus.OK);
+	}
+}
