3968def9a85288277e94de60e62f36984b0431f7
dawidcwiek <cwiekdawid1@gmail.com>
2020-06-17 16:41:44 +0200

    chart

--- app/src/Pages/AdminPage.js
+++ app/src/Pages/AdminPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import RootTemplate from 'templates/RootTemplate';
 import CategoryList from 'components/Category/CategoryList';
+import Chart from 'components/Chart/Chart';
 
 const AdminPage = () => {
   return (
     <RootTemplate>
+      <Chart />
       <CategoryList />
     </RootTemplate>
   );
